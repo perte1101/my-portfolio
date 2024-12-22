f8d3e7187885f40eedece539b5a76a4d348661be
DESKTOP-6IKT6T7\KimSeoYeon <perte1101@gmail.com>
2024-12-22 23:09:12 +0900

    Fixed embedded repository issue

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+        <p>This is my portfolio website, built with Next.js and Tailwind CSS.</p>
+      </div>
+    );
+  }
