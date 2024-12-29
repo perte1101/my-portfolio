c70074e0b91d85f8498aa2791bd79c6a7b4d7055
perte1101 <kimsmj1101@gmail.com>
2024-12-29 23:37:14 +0900

    test commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="bg-blue-500 text-white min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
+        <h1 className="text-4xl font-bold">Welcome to My Portfolio.</h1>
       </div>
     );
   }
